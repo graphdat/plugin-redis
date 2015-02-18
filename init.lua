f6964d4e0b3e9e62dcb9f9c20c52077b4e85acbf
Ivano Picco <ivano.picco@pianobit.com>
2015-02-18 10:09:30 +0000

    Code reorgs

--- init.lua
+++ init.lua
@@ -2,12 +2,12 @@
 -- [author] Ivano Picco <ivano.piccopianobit.com>
 
 -- Requires.
-local redis = require('luvit-redis')
-local string= require('string')
-local split = require('split')
-
 local utils = require('utils')
 local uv_native = require ('uv_native')
+local string = require('string')
+local split = require('split')
+local redis = require('luvit-redis')
+local timer = require('timer')
 
 -- Configuration.
 local _param = require ('./param')
@@ -17,8 +17,8 @@ _param.pollInterval = (_param.pollInterval>0) and _param.pollInterval or 10000;
 local client = redis:new(_param.port, _param.host)
 
 client:on("error", function (err)
-    utils.debug("Error (error callback): ", err)
-end)
+  utils.debug("Error (error callback): ", err)
+  end)
 
 if (_param.password) then 
   client:auth(_param.password) 
@@ -44,6 +44,18 @@ function parseEachLine(line)
   end
 end
 
+-- print results
+function outputs()
+  utils.print('REDIS_CONNECTED_CLIENTS', currentValues.connected_clients, _param.source)
+  utils.print('REDIS_KEY_HITS', diffvalues('keyspace_hits'), _param.source)
+  utils.print('REDIS_KEY_MISSES', diffvalues('keyspace_misses'), _param.source)
+  utils.print('REDIS_KEYS_EXPIRED', diffvalues('expired_keys'), _param.source)
+  utils.print('REDIS_KEY_EVICTIONS', diffvalues('evicted_keys'), _param.source)
+  utils.print('REDIS_COMMANDS_PROCESSED', diffvalues('total_commands_processed'), _param.source)
+  utils.print('REDIS_CONNECTIONS_RECEIVED', diffvalues('total_connections_received'), _param.source)
+  utils.print('REDIS_USED_MEMORY', currentValues.used_memory_rss / uv_native.getTotalMemory(), _param.source)
+end
+
 -- Get current values.
 function poll()
   client:info( function(err,result)
@@ -53,21 +65,9 @@ function poll()
     end
     -- call func with each word in a string
     result:gsub("[^\r\n]+", parseEachLine)
-    utils.prettyPrint(currentValues);
-    utils.prettyPrint(uv_native.getTotalMemory());
-    --outputs
-    utils.print('REDIS_CONNECTED_CLIENTS', currentValues.connected_clients, _param.source)
-    utils.print('REDIS_KEY_HITS', diffvalues('keyspace_hits'), _param.source)
-    utils.print('REDIS_KEY_MISSES', diffvalues('keyspace_misses'), _param.source)
-    utils.print('REDIS_KEYS_EXPIRED', diffvalues('expired_keys'), _param.source)
-    utils.print('REDIS_KEY_EVICTIONS', diffvalues('evicted_keys'), _param.source)
-    utils.print('REDIS_COMMANDS_PROCESSED', diffvalues('total_commands_processed'), _param.source)
-    utils.print('REDIS_CONNECTIONS_RECEIVED', diffvalues('total_connections_received'), _param.source)
-    utils.print('REDIS_USED_MEMORY', currentValues.used_memory_rss / uv_native.getTotalMemory(), _param.source)
-  end)
+    end)
 end
 
 -- Ready, go.
 poll()
-local timer = require('timer')
 timer.setInterval(_param.pollInterval,poll)
